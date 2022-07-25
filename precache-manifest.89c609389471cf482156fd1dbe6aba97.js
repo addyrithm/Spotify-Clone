d12b5311ae7658ab5eeb7395e12191fc3cf4a29b
addyrithm <adarshsharma3.143@gmail.com>
2022-07-25 10:16:59 +0530

    Updates

---
+++ precache-manifest.89c609389471cf482156fd1dbe6aba97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "164a6d1db4409f5235f7",
+    "url": "/TextUtils-React/static/css/main.83ae7409.chunk.css"
+  },
+  {
+    "revision": "164a6d1db4409f5235f7",
+    "url": "/TextUtils-React/static/js/main.164a6d1d.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/TextUtils-React/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "0d79c3c2a5762a692cdb",
+    "url": "/TextUtils-React/static/js/runtime~main.0d79c3c2.js"
+  },
+  {
+    "revision": "3563f9d368b6d08679c97f892436824a",
+    "url": "/TextUtils-React/index.html"
+  }
+];
